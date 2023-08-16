917845f400be9986e5ab2ed2afd5dd797e133915
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2023-08-16 16:21:45 +0100

    add:nav

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,4 +1,5 @@
 import Close from '../../assets/shared/icon-close.svg';
+import { Link } from 'react-router-dom';
 
 type Nav = {
     isOpen: boolean
@@ -11,6 +12,20 @@ export default function MobileNav({onClose, isOpen}: Nav) {
     <div className='absolute top-0 left-36 bg-transparent w-64 backdrop-blur-2xl' style={{height:'844px'}}>
         <div>
             <img src={Close} className='absolute top-8 left-48' onClick={onClose}/>
+           <div className='flex flex-col mt-36 gap-9'>
+            <Link to='/' className='text-white font-barlowCondensed font-thin tracking-wide mx-8 flex gap-3'>
+                <span className='font-bold'>00</span> HOME
+            </Link>
+            <Link to='/' className='text-white font-barlowCondensed font-thin tracking-wide mx-8 flex gap-3'>
+                <span className='font-bold'>01</span> DESTINATION
+            </Link>
+            <Link to='/' className='text-white font-barlowCondensed font-thin tracking-wide mx-8 flex gap-3'>
+                <span className='font-bold'>02</span> CREW
+            </Link>
+            <Link to='/' className='text-white font-barlowCondensed font-thin tracking-wide mx-8 flex gap-3'>
+                <span className='font-bold'>03</span> TECHNOLOGY
+            </Link>
+           </div>
         </div>
     </div>
   ) : null
